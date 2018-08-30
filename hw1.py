bb158c907678d9f54b7cc150aa4ee9990eeef9c0
Zackh Tucker <zrt4026@live.missouristate.edu>
2018-08-29 19:48:19 -0500

    Commented and ready to turn in

--- hw1.py
+++ hw1.py
@@ -23,16 +23,32 @@ global ai_win_dict
 ai_win_dict = {}
 
 def ai_choice(x):
+    """
+    This function takes in the number of sticks left in the game, 
+    represented by x, and produces a random play given what numbers
+    are in that specific bin. 
+    """
     ai_selection = random.choice(AIbins[x])
     return ai_selection
 
+
 def human_choice(x):
+    """
+    This produces a random choice for the human player, simply 
+    by making a list and using random.choice.
+    """
     plays = [1,2,3]
     human_selection = random.choice(plays)
     return human_selection
 
+
 def main_game_loop(NumOfSticks, player):
-    player = random.choice([1, 2]) #1 is human, 2 is AI...this is to swap players at the end of a given turn
+    """
+    This function holds the main logic of the game, as well as maintains
+    the dictionary that holds the moves made by the AI. It returns the winner, 
+    to be used by the main function to dictate how to continue. 
+    """
+    player = random.choice([1, 2]) #1 is human, 2 is AI...this is to swap players
     while NumOfSticks >= 0:
         if player == 2:
             #This is the ai player loop
@@ -73,25 +89,40 @@ def main_game_loop(NumOfSticks, player):
             player = 2
     return winner       
 
+
 def increase_odds(dict):
+    """
+    This function is the heart of the AI. By adding the moves made
+    in a winning game to the set of potential choices, it increases 
+    the likelihood that a "good" choice is made.
+    """
     for x in dict:
         AIbins[x].append(dict[x])
     return AIbins
 
+
 def print_bins(bin):
+    """
+    This function simply prints out the data. Counter is a library 
+    that creates a dictionary where the values themselves are keys, 
+    and the number of occurences is the value. Counter is O(n) as 
+    opposed to the built in .count(x) which is O(n^2)
+    """
     for i in range(1, len(bin)): 
         c = Counter(bin[i])
         print(i, " ",  c)
             
 def main():
+    """
+    Main keeps track of how many games the AI wins in the simulation.
+    """
     ai_win_total = 0
     for i in range(0, NumofGames) :
         winner = main_game_loop(NumOfSticks, 1)
         if winner == 2:
             ai_win_total += 1
-            increase_odds(ai_win_dict) #this function is going to loop through the dict and add winning moves
+            increase_odds(ai_win_dict)
         else:
-            #print ("Human Won!")
             ai_win_dict.clear()
         i += 1
     print_bins(AIbins)
